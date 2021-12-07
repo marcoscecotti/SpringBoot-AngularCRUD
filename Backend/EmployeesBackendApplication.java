ad9c51fbb3c02ffdcec772a3a473a047d7ac860b
Marcos José Cecotti <marcosjcecotti@hotmail.com>
2021-12-06 21:25:32 -0300

    Update EmployeesBackendApplication.java

--- Backend/EmployeesBackendApplication.java
+++ Backend/EmployeesBackendApplication.java
@@ -1,1 +1,13 @@
+package com.example.employeesBackend;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesBackendApplication.class, args);
+	}
+
+}
